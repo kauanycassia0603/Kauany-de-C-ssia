55238626b36654492fb485040cb37329b4d9373c
kauanycassia0603 <125677350+kauanycassia0603@users.noreply.github.com>
2024-11-26 08:53:31 -0300

    Add files via upload

---
+++ Kauany gráfico/graficos/informacoesGlobais.js
@@ -0,0 +1,20 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas) * 100)
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed(2)
+
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `As redes sociais são sites e aplicativos que operam em níveis diversos — como profissional, de relacionamento, dentre outros — mas sempre permitindo o compartilhamento de informações entre pessoas e/ou empresas.`
+
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
+}
+
+vizualizarInformacoesGlobais()
